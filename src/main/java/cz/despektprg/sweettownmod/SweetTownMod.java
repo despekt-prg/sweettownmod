369d7816bd71f523029365115aeecf9c7d5b7de7
despekt-prg <despekt@seznam.cz>
2025-02-02 18:15:25 +0100

    first item

--- src/main/java/cz/despektprg/sweettownmod/SweetTownMod.java
+++ src/main/java/cz/despektprg/sweettownmod/SweetTownMod.java
@@ -1,17 +1,18 @@
 package cz.despektprg.sweettownmod;
 
+import cz.despektprg.sweettownmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //very important pokus
 public class SweetTownMod implements ModInitializer {
-	public static final String MOD_ID = "sweet-town-mod";
+	public static final String MOD_ID = "sweettownmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.RegisterModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/cz/despektprg/sweettownmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package cz.despektprg.sweettownmod.item;
+
+import cz.despektprg.sweettownmod.SweetTownMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item GINGERBREAD_MAN = registerItem("gingerbread_man", new Item(new Item.Settings()));
+
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(SweetTownMod.MOD_ID, name), item);
+
+    }
+
+    public static void RegisterModItems() {
+        SweetTownMod.LOGGER.info("Registering items for " + SweetTownMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(GINGERBREAD_MAN);
+        });
+    }
+}
