884572036a9349349c51f25b5ea7ed7170fd5b8a
despekt-prg <despekt@seznam.cz>
2025-02-04 23:55:39 +0100

    updates for new items

--- src/main/java/cz/despektprg/sweettownmod/item/ModItems.java
+++ src/main/java/cz/despektprg/sweettownmod/item/ModItems.java
@@ -11,6 +11,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item GINGERBREAD_MAN = registerItem("gingerbread_man", new Item(new Item.Settings()));
     public static final Item SPICE = registerItem("spice", new Item(new Item.Settings()));
+    public static final Item LICORICE_TREE_SAPLING = registerItem("licorice_tree_sapling", new Item(new Item.Settings()));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(SweetTownMod.MOD_ID, name), item);
@@ -23,6 +24,10 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(GINGERBREAD_MAN);
             entries.add(SPICE);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(LICORICE_TREE_SAPLING);
         });
     }
 }
