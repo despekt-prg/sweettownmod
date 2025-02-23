abd8bb22a95a27f6d10d92df4a06447fc0b08c5d
despekt-prg <despekt@gmail.com>
2025-02-23 11:21:34 +0100

    test commit from another PC

--- src/main/java/cz/despektprg/sweettownmod/SweetTownMod.java
+++ src/main/java/cz/despektprg/sweettownmod/SweetTownMod.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+/* test change in GIT */
 public class SweetTownMod implements ModInitializer {
 	public static final String MOD_ID = "sweettownmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
